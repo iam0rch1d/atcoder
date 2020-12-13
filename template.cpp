2179211d696252f3fdea11d9c0fab813af1e2f01
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-12-14 05:21:16 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -12,9 +12,32 @@ using pii = pair<int, int>;
 #define IFOR(i, x, y) for (int i = y; i > x; i--)
 #define IFOR_(i, x, y) for (int i = y; i >= x; i--)
 #define REP(i, x) FOR(i, 0, x)
+#define DEBUG(x) cerr << "\033[1;35m" << x << "\033[0m\n"
 
-template<typename T> void chMax(T &m, const T &q) { m = max(m, q); }
-template<typename T> void chMin(T &m, const T &q) { m = min(m, q); }
+template<typename T>
+void chMax(T &m, const T &q) { m = max(m, q); }
+
+template<typename T>
+void chMin(T &m, const T &q) { m = min(m, q); }
+
+template<typename T1, typename T2>
+pair<T1, T2> operator+(const pair<T1, T2> &l, const pair<T1, T2> &r) {
+    return make_pair(l.first + r.first, l.second + r.second);
+}
+
+template<typename T1, typename T2>
+pair<T1, T2> operator-(const pair<T1, T2> &l, const pair<T1, T2> &r) {
+    return make_pair(l.first - r.first, l.second - r.second);
+}
+
+template<typename T>
+T square(T x) { return x * x; }
+
+template<typename T>
+T manhattan(pair<T, T> a, pair<T, T> b) { return abs(b.first - a.first) + abs(b.second - a.second); }
+
+template<typename T>
+T euclidean(pair<T, T> a, pair<T, T> b) { return square(b.first - a.first) + square(b.second - a.second); }
 
 int main() {
     //ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
